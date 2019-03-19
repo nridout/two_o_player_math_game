9eafacf436f810aebcf23242f9915d55b0060aca
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-19 12:06:25 -0400

    made lives readable

--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 class Player
   attr_reader :name
+  attr_reader :lives
 
   # Each player starts with 3 lives
   def initialize(name)
@@ -22,7 +23,7 @@ end
 
 # TESTS
 
-player1 = Player.new('Player 1')
-p player1
-player1.lose_life
-p player1
+# player1 = Player.new('Player 1')
+# p player1
+# player1.lose_life
+# p player1

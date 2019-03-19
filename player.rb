a476837ef42f9525bd0635f8a9ae122318658be9
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-18 21:56:06 -0400

    Mapped out initial player functionality

---
+++ player.rb
@@ -0,0 +1,28 @@
+class Player
+  attr_reader :name
+
+  # Each player starts with 3 lives
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  # manage lives left (lose life if question is mis-answered)
+  def lose_life
+    @lives -= 1
+  end
+
+  #player lost => once no lives left
+  def player_lost?
+    @lives = 0
+    puts "#{name} lost"
+  end
+end
+
+
+# TESTS
+
+player1 = Player.new('Player 1')
+p player1
+player1.lose_life
+p player1

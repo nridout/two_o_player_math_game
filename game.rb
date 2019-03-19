ab01afbc8766441bef8071ffa7891865909f5089
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-19 12:06:08 -0400

    Added question & answer flow

--- game.rb
+++ game.rb
@@ -1,53 +1,62 @@
 require_relative 'player'
+require_relative 'question'
 
 class Game
 
   # Initialize Players
   def initialize
-    player1 = Player.new('Player 1')
-    player2 = Player.new('Player 2')
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
   end
 
+  # Handles the game flow
   def play
-    while (!game_over?)
-      #Whose turn (Player 1 or Player 2)
-      # turn = ??
-      current_player = player1 # need to handle turns
-      question = "What does 5 plus 3 equal?"
-      answer = "8"
 
-      puts "#{current_player.name}: #{question}"
+    # turns keeps track of the current player
+    current_player = @player1
 
-      # ask for CL input ??
+    # Initialize a new question
+    question = Question.new
+    question.new_question
 
-      # accept input from command line = gets.chomp
-      answer = gets.chomp
+    puts "#{current_player.name}: #{question.question}"
 
-      # validate if the answer is_correct?
-      def is_correct?
-        answer == 8
-      end
+    # Accept user answer & convert to number
+    print "> "
+    answer = $stdin.gets.chomp.to_i
+    # answer = 4
+    # puts answer
 
-      # chose response depending on correctness of answer
-      if answer is_correct?
-        puts "#{current_player.name}: Seriously? No!"
-      else
-        puts "#{current_player.name}: YES! You are correct"
-      end
-
-      # Output status summary
-      puts "P1: #{player1.lives}/3 vs #{player2.lives}/3"
-
-      # Game ends when all lives are lost
-      if current_player player_lost?
-        # Game over
-        # Announce who won
-      else
-        # New turn
-      end
+    # Validate Answer
+    if answer == question.answer
+      puts "#{current_player.name}: YES! You are correct"
+    else
+      puts "#{current_player.name}: Seriously? No!"
+      current_player.lose_life
     end
+
+    # Output status summary
+    puts "P1: #{@player1.lives}/3 vs #{@player2.lives}/3"
+    puts "----- NEW TURN -----"
+
   end
 end
+
+
+#     while (!game_over?)
+#       #Whose turn (Player 1 or Player 2)
+#       # turn = ??
+
+#       # Game ends when all lives are lost
+#       if current_player player_lost?
+#         # Game over
+#         # Announce who won
+#       else
+#         # New turn
+#       end
+#     end
+#   end
+# end
 
 
 

1e7735169289448c5eeb2408b290b65e2095a3f0
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-19 12:45:29 -0400

    Cleaned up code & recommented

--- question.rb
+++ question.rb
@@ -1,6 +1,3 @@
-# - Generates question & answer
-# - Random numbers
-
 class Question
   attr_reader :answer, :question
 
@@ -9,19 +6,14 @@ class Question
     @answer = 0
   end
 
+  # Generate new game question
   def new_question
+    # Two random numbers
     random_num1 = 1 + rand(10)
     random_num2 = 1 + rand(10)
-    @answer = (random_num1 + random_num2)
+    # Store question in format: What does 5 plus 3 equal?
     @question = "What does #{random_num1} plus #{random_num2} equal?"
+    # Store answer
+    @answer = (random_num1 + random_num2)
   end
-
 end
-
-# # Tests
-
-# # #What does 5 plus 3 equal?
-# question1 = Question.new
-# p question1.new_question
-# p question1
-# puts question1.question
